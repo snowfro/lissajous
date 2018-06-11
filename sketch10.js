0703a7ffb60c7af2e1bd57f30bf66c8b5c8cb1e1
snowfro <erick@snowfro.com>
2018-06-11 16:55:10 -0500

    Add files via upload

---
+++ sketch10.js
@@ -0,0 +1,155 @@
+// M_2_5_02
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * explore different parameters of drawing lissajous figures
+ *
+ * KEYS
+ * s                   : save png
+ */
+
+'use strict';
+
+var sketch = function( p ) {
+
+  var pointCount = 1000;
+  var pointIndex = 0;
+  var lissajousPoints = [];
+
+  var freqX = allSlices[1]/30;
+  console.log(allSlices[1]);
+  console.log(freqX);
+  var freqY = allSlices[2]/30;
+  var phi = allSlices[0];
+
+
+  var modFreqX = allSlices[3]/100;
+  var modFreqY = allSlices[4]/100;
+
+  var modFreq2X = allSlices[5]/100;
+  var modFreq2Y = allSlices[6]/100;
+  var modFreq2Strength = allSlices[7]/100;
+
+  var randomOffset = allSlices[8]/100;
+if (allSlices[9]<125){
+  var invertBackground = false;} else { var invertBackground = true;}
+  console.log(invertBackground);
+  var lineWeight = 1;
+  var lineAlpha = 20;
+
+  var connectAllPoints = true;
+  var connectionRadius = 110;
+  var minHueValue = allSlices[11];
+  console.log("minHue" + minHueValue);
+  var maxHueValue = allSlices[12];
+  console.log("maxHue" + maxHueValue);
+  //var saturationValue = allSlices[13]/2.55;
+if (!invertBackground){
+  var saturationValue = 100;} else {var saturationValue = 80;}
+  console.log("satslice: " + allSlices[13]);
+  console.log("satvalue: " + saturationValue);
+  //var brightnessValue = allSlices[14]/2.55;
+if (!invertBackground){var brightnessValue = 100;} else {var brightnessValue = 0;}
+  console.log("brtvalue: " + brightnessValue);
+  var invertHue = false;
+
+  p.setup = function() {
+
+    p.createCanvas(800, 800);
+
+    p.colorMode(p.HSB, 360, 100, 100, 100);
+
+    p.strokeWeight(lineWeight);
+    p.noFill();
+
+    var backgroundColor = invertBackground ? p.color(0) : p.color(0, 0, 100);
+    p.background(backgroundColor);
+
+
+    calculateLissajousPoints();
+  }
+
+
+
+  p.draw = function() {
+
+    if (!connectAllPoints) {
+      for (var i = 0; i < pointCount - 1; i++) {
+        drawLine(lissajousPoints[i], lissajousPoints[i + 1]);
+      }
+    } else {
+      // Don't draw all lines at once. Draw them in steps of 10 milliseconds to improve performance.
+      var drawEndTime = window.performance.now() + 10;
+      for (var i1 = pointIndex; i1 < pointCount && window.performance.now() < drawEndTime; i1++) {
+        for (var i2 = 0; i2 < i1; i2++) {
+          drawLine(lissajousPoints[i1], lissajousPoints[i2]);
+        }
+        pointIndex = i1;
+      }
+    }
+
+    if (pointIndex >= pointCount - 1) {
+      p.noLoop();
+    }
+  }
+
+  p.keyPressed = function() {
+    if (p.key == 's' || p.key == 'S') {
+      p.saveCanvas(gd.timestamp(), 'png');
+    }
+  }
+
+  var calculateLissajousPoints = function() {
+    p.randomSeed(0);
+
+    for (var i = 0; i <= pointCount; i++) {
+      var angle = p.map(i, 0, pointCount, 0, p.TAU);
+
+      var fmx = p.sin(angle * modFreq2X) * modFreq2Strength + 1;
+      var fmy = p.sin(angle * modFreq2Y) * modFreq2Strength + 1;
+
+      var x = p.sin(angle * freqX * fmx + p.radians(phi)) * p.cos(angle * modFreqX);
+      var y = p.sin(angle * freqY * fmy) * p.cos(angle * modFreqY);
+
+      var rx = p.random(-randomOffset, randomOffset);
+      var ry = p.random(-randomOffset, randomOffset);
+
+      x = (x * (p.width / 2 - 30 - randomOffset) + p.width / 2) + rx;
+      y = (y * (p.height / 2 - 30 - randomOffset) + p.height / 2) + ry;
+
+      lissajousPoints[i] = p.createVector(x, y);
+    }
+  }
+
+  var drawLine = function(vector1, vector2) {
+    var distance = p5.Vector.dist(vector1, vector2);
+    var angle = p.pow(1 / (distance / connectionRadius + 1), 6);
+
+    if (distance <= connectionRadius) {
+      var hue = p.lerp(minHueValue, maxHueValue, (invertHue ? 1 - angle : angle)) % 360;
+      p.stroke(
+        hue,
+        saturationValue,
+        invertBackground ? 100 - brightnessValue : brightnessValue,
+        angle * lineAlpha + (pointIndex % 2 * 2));
+      p.line(vector1.x, vector1.y, vector2.x, vector2.y);
+    }
+  }
+
+};
---
+++ sketch6.js
@@ -0,0 +1,111 @@
+
+var key = "abcdef0123456789";
+var xhr = new XMLHttpRequest();
+let allSlices = [];
+let allSlicesSmall = [];
+let hash = "0x0";
+let responseOld = "";
+
+
+
+function pingContract() {
+xhr.open('GET', "http://redeemr.whatsthescore.webfactional.com/", true);
+xhr.send();
+
+xhr.onreadystatechange = processRequest;}
+
+function convertRange( value, r1, r2 ) {
+    return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
+}
+
+
+function hexToRgb(hex) {
+    var bigint = parseInt(hex, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+
+    return r + "," + g + "," + b;
+}
+
+function hexToDec(hex) {
+  var smallInt = parseInt(hex, 16);
+  return smallInt;
+}
+
+window.setInterval(function(){
+  pingContract()
+}, 500);
+
+function processRequest() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        //document.open();
+  
+        var response = xhr.responseText;
+
+        if (responseOld != response) {
+
+        //let testTop = 0;
+        //let testMid = 0;
+        //let testBot = 0;
+        allSlices = [];
+        allSlicesSmall = [];
+        //rgbSlices = [];
+        //hash = response;
+
+
+        console.log(response);
+        //document.write("Hash is " + response + "<br>");
+
+          //let rgbvals = response.slice(5,11);
+          //document.body.style.backgroundColor = rgbvals;
+
+          for (let i = 2; i<response.length; i = i+2) {
+            let hexSlice = response.slice(i,i+2);
+            let sliceRGB = hexToRgb(hexSlice);
+            let sliceDec = hexToDec(hexSlice);
+            let sliceDecSmall = convertRange(sliceDec, [0,255], [0,1] );
+            let sliceRange = convertRange(sliceDec, [0,255], [1,100]);
+            allSlices.push(sliceDec);
+            allSlicesSmall.push(sliceDecSmall);
+}
+
+
+           /*for (let i = 2; i<response.length; i = i+2) {
+            let hexSlice = response.slice(i,i+2);
+            let sliceDec = hexToDec(hexSlice);
+            allSlices.push(sliceDec);
+}
+for (let i = 2; i<response.length; i = i+2) {
+  let hexSlice = response.slice(i,i+2);
+  let sliceDec = hexToDec(hexSlice);
+  allSlices.push(sliceDec);
+}
+for (let i = 2; i<response.length; i = i+2) {
+  let hexSlice = response.slice(i,i+2);
+  let sliceDec = hexToDec(hexSlice);
+  allSlices.push(sliceDec);
+}
+for (let i = 2; i<response.length; i = i+2) {
+  let hexSlice = response.slice(i,i+2);
+  let sliceDec = hexToDec(hexSlice);
+  allSlices.push(sliceDec);
+} */
+
+
+
+//document.write("Low: " + testTop + "<br>");
+//document.write("Mid: " + testMid + "<br>");
+//document.write("Top: " + testBot + "<br>");
+
+//for (let i = 0; i<allSlices.length; i++){
+
+
+//document.write(allSlices[i] + "<br>");}
+
+var myp5 = new p5(sketch);
+
+}
+responseOld = response;
+    }
+}
